7091bcaed640e8efa2c1aca31a1ff374bb77e42f
Dwip Lahare <123765492+Dwip272004@users.noreply.github.com>
2025-05-23 02:01:47 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,5 +1,8 @@
+import os
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("logger-f9c87-firebase-adminsdk-fbsvc-26f12e5bb9.json")
+# Example: Render mounts secret to /etc/secrets/firebase.json
+cred_path = "/etc/secrets/logger-f9c87-firebase-adminsdk-fbsvc-26f12e5bb9.json"  # Change to your mounted secret path
+cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
